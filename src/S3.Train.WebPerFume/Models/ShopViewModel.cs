9f9fff6c7eafe024f50cc035d3ea9b60ed11ccfb
chuongvo0310 <chuongvh310@gmail.com>
2020-03-09 16:10:39 +0700

    model for shopview

---
+++ src/S3.Train.WebPerFume/Models/ShopViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using S3Train.Model.Brand;
+using S3Train.Model.Category;
+using S3Train.Model.Product;
+
+namespace S3.Train.WebPerFume.Models
+{
+    public class ShopViewModel
+    {
+        public IList<CategoryModel> categoryModels;
+        public IList<ProductModel> productModels;
+        public IList<BrandModel> brandModels;
+    }
+}
